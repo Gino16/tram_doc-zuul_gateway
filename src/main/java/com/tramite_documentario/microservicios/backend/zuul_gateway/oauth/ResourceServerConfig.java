2e10d5dfef58c700121948b90a32dfe6f66dfdef
Gino Ascencio <gino.francisco@hotmail.com>
2021-02-28 00:15:32 -0500

    enviar archivo en post permitido

--- src/main/java/com/tramite_documentario/microservicios/backend/zuul_gateway/oauth/ResourceServerConfig.java
+++ src/main/java/com/tramite_documentario/microservicios/backend/zuul_gateway/oauth/ResourceServerConfig.java
@@ -32,7 +32,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/api/security/**", "/api/usuarios/forgot_password", "/api/usuarios/cambiar_contrasenia", "/api/solicitudes/exportar/{id}/", "/api/archivos/ver-archivo/{id}/").permitAll()
                 .antMatchers(HttpMethod.GET, "/verificarSesion").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.GET, "/api/solicitudes", "/api/solicitudes/{id}", "/api/archivos/{id}", "/api/archivos/ver-archivo/{id}", "/api/archivos/enviar-archivo/{id}").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.GET, "/api/solicitudes", "/api/solicitudes/{id}", "/api/archivos/{id}", "/api/archivos/ver-archivo/{id}").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.POST, "/api/archivos/enviar-archivo/{id}").hasAnyRole("ADMIN", "USER")
                 .antMatchers(HttpMethod.GET, "/api/personas", "/api/personas/puestos", "/api/archivos", "/api/archivos/tipoArchivos", "/api/archivos/archivos-sin-solicitud").hasRole("ADMIN")
                 .antMatchers(HttpMethod.GET, "/api/personas/{id}", "/api/personas/buscar-por-codEstudiante/{codigo}", "/api/personas/buscar-por-email/{correo}", "/api/personas/buscar-por-dni/{dniRuc}", "/api/personas/buscar-por-nombre/{nombre}", "/api/personas/buscar-por-apellido/{apellido}", "/api/solicitudes/exportar/{id}").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/api/solicitudes", "/api/personas", "/api/archivos", "/api/archivos/crear-con-file").hasRole("ADMIN")
